c73b65c5f540422640b33b3dfe8be569e9ebf755
Faraildes <faraildesbarbosaferreira@gmail.com>
2024-08-19 22:35:07 -0300

    Student entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import model.entities.Student;
 import model.entities.Teacher;
 
 public class Program {
@@ -9,7 +10,11 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Teacher obj = new Teacher(1, "Francisco", new Date(), "12345678921", "987456231", 12000.00 );
+		
+		Student student = new Student(21, "Bob", new Date(), "12345678954", "985647521", "10");
+		
 		System.out.println(obj);
+		System.out.println(student);
 
 	}
 
